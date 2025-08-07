80ff9e079af3a221f352f1d4c5b04ad2bd883573
Felicia D OGarro <fdogarro@gmail.com>
2025-08-07 14:45:59 -0400

    remove alembic config

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -15,7 +15,7 @@ load_dotenv()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
-config = context.config
+# config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
